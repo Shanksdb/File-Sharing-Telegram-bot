b7fc400ab6565f1bad3aa3f398b1b889ca78298b
𝐀𝐢𝐳𝐞𝐧-𝐒𝐚𝐦𝐚 <yashpacholi895@gmail.com>
2024-11-27 09:26:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/c/1987260753/248")
 #put TRUE if you want this feature
 USE_PAYMENT = True if (True if os.environ.get("USE_PAYMENT", "TRUE") == "TRUE" else False) and (USE_SHORTLINK) else False
 #UPI ID
-UPI_ID = os.environ.get("UPI_ID", "yashpirateking")
+UPI_ID = os.environ.get("UPI_ID", "yashpirateking@fam")
 #UPI QR CODE IMAGE
 UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "https://t.me/LUFFY1JOYBOY")
 #SCREENSHOT URL of ADMIN for verification of payments
