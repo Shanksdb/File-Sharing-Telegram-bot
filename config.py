b26d983a36e900f4de8fe5546e98f6e405aea98e
𝐀𝐢𝐳𝐞𝐧-𝐒𝐚𝐦𝐚 <yashpacholi895@gmail.com>
2024-12-03 15:19:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,7 +83,7 @@ USE_PAYMENT = True if (True if os.environ.get("USE_PAYMENT", "TRUE") == "TRUE" e
 #UPI ID
 UPI_ID = os.environ.get("UPI_ID", "8979194557@mbk")
 #UPI QR CODE IMAGE
-UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "https://envs.sh/Y5d.jpg")
+UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "https://t.me/Saitama_One_Punchh")
 #SCREENSHOT URL of ADMIN for verification of payments
 SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/{OWNER_TAG}")
 #Time and its price
